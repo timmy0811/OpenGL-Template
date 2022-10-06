f561bf8c928189a168cda173f8504428a5a6fae5
Timm <timmschmidt7@t-online.de>
2022-10-06 19:38:52 +0200

    Linked GLEW

--- OpenGL-Learning/main.cpp
+++ OpenGL-Learning/main.cpp
@@ -1,4 +1,7 @@
+#include <GLEW/glew.h>
 #include <GLFW/glfw3.h>
+
+#include <iostream>
 
 int main(void)
 {
@@ -19,11 +22,26 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    if (glewInit() != GLEW_OK) {
+        std::cout << "Could not init glew." << std::endl;
+        return -1;
+    }
+
+    std::cout << glGetString(GL_VERSION) << std::endl;
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+        
+        glBegin(GL_TRIANGLES);
+
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(0.0f, 0.5f);
+        glVertex2f(0.5f, -0.5f);
+
+        glEnd();
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
